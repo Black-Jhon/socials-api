0b1f8f8e9f26e85c00059416d9e19110149ae5c5
Chidiebere <chukwuchidieberejohn@gmail.com>
2022-08-26 08:49:05 +0100

    User Auth

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,39 +1,73 @@
 // @ts-nocheck
 import UserModel from "../Models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Registering a New User
 export const registerUser = async (req, res) => {
-    const {username, password, firstname, lastname} = req.body;
+  // const {username, password, firstname, lastname} = req.body;
 
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = new UserModel({username, password: hashedPassword, firstname, lastname});
+  req.body.password = hashedPassword;
 
-    try {
-        await newUser.save();
-        res.status(201).json(newUser);
-    } catch (error) {
-        res.status(500).json({message: error.message});
+  const newUser = new UserModel(req.body);
+
+  const { username } = req.body;
+
+  try {
+    const oldUser = await UserModel.findOne({ username });
+
+    if (oldUser) {
+      return res.status(400).json({ message: "Username is already registered!" });
     }
+
+    const user = await newUser.save();
+
+    // Jwt Auth
+    const token = jwt.sign(
+      {
+        username: user.username,
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "1hr" }
+    );
+
+    res.status(201).json({ user, token }); // This gets stored inside local storage and redux store
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // Login User
 export const loginUser = async (req, res) => {
-    const {username, password} = req.body;
+  const { username, password } = req.body;
 
-    try {
-        const user = await UserModel.findOne({username: username});
+  try {
+    const user = await UserModel.findOne({ username: username });
 
-        if (user) {
-            const validity = await bcrypt.compare(password, user.password);
-            validity ? res.status(200).json(user) : res.status(400).json("Wrong password");
-        } else {
-            res.status(404).json("User does not exist.");
-        }
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
 
-    } catch (error) {
-        res.status(500).json({message: error.message});
+      if (!validity) {
+        res.status(400).json({ message: "Wrong password!" });
+      } else {
+        const token = jwt.sign(
+          {
+            username: user.username,
+            id: user._id,
+          },
+          process.env.JWT_KEY,
+          { expiresIn: "1hr" }
+        );
+        res.status(200).json({ user, token });
+      }
+    } else {
+      res.status(404).json("User does not exist.");
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
